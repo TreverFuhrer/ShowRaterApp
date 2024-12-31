f274eb33b152387390c23719c939696a19b3497d
toki <clevertrever31@gmail.com>
2024-12-30 18:04:36 -0600

    Initial Commit

---
+++ ShowRater/ShowRaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowRaterApp.swift
+//  ShowRater
+//
+//  Created by Toki on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ShowRaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShowRaterTests/ShowRaterTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShowRaterTests.swift
+//  ShowRaterTests
+//
+//  Created by Toki on 12/30/24.
+//
+
+import Testing
+@testable import ShowRater
+
+struct ShowRaterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
